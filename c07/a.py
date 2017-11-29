3a862ce16da3bc36e4d009408d29244524cd9df9
xusong <xusong@stevegame.cn>
2017-11-29 11:57:47 +0800

    .

---
+++ c07/a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Student(object):
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def print_score(self):
+        print '%s: %s' % (self.__name, self.__score)
+
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+bart = Student('Bart Simpson', 59)
+bart.print_score()
+print(bart.get_grade())
